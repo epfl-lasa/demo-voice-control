2cd21d1355350f94092d7007203963119fa117b2
nbfigueroa <nbfigueroa@gmail.com>
2018-09-04 23:43:57 +0200

    Defined new reduced vocabulary and publish commands as messages

--- src/test_voice_control.py
+++ src/test_voice_control.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
-"""This module is a simple demonstration of voice control
-for ROS turtlebot using pocketsphinx
+"""This module is a simple demonstration of voice control for ROS 
 """
-
 import argparse
 import roslib
 import rospy
 
-from geometry_msgs.msg import Twist
-
+from std_msgs.msg import String
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 import pyaudio
@@ -27,14 +24,13 @@ class ASRControl(object):
     """
     def __init__(self, model, lexicon, kwlist, pub):
         # initialize ROS
-        self.speed = 0.2
-        self.msg = Twist()
+        self.msg = String
 
-        rospy.init_node('voice_cmd_vel')
+        rospy.init_node('test_voice_cmd')
         rospy.on_shutdown(self.shutdown)
 
         # you may need to change publisher destination depending on what you run
-        self.pub_ = rospy.Publisher(pub, Twist, queue_size=10)
+        self.pub_ = rospy.Publisher(pub, String, queue_size=10)
 
         # initialize pocketsphinx
         config = Decoder.default_config()
@@ -69,45 +65,28 @@ class ASRControl(object):
             self.decoder.end_utt()
             self.decoder.start_utt()
             # you may want to modify the main logic here
-            if seg.word.find("full speed") > -1:
-                if self.speed == 0.2:
-                    self.msg.linear.x = self.msg.linear.x*2
-                    self.msg.angular.z = self.msg.angular.z*2
-                    self.speed = 0.4
-            if seg.word.find("half speed") > -1:
-                if self.speed == 0.4:
-                    self.msg.linear.x = self.msg.linear.x/2
-                    self.msg.angular.z = self.msg.angular.z/2
-                    self.speed = 0.2
-            if seg.word.find("forward") > -1:
-                self.msg.linear.x = self.speed
-                self.msg.angular.z = 0
-            elif seg.word.find("left") > -1:
-                if self.msg.linear.x != 0:
-                    if self.msg.angular.z < self.speed:
-                        self.msg.angular.z += 0.05
-                else:
-                    self.msg.angular.z = self.speed*2
-            elif seg.word.find("right") > -1:
-                if self.msg.linear.x != 0:
-                    if self.msg.angular.z > -self.speed:
-                        self.msg.angular.z -= 0.05
-                else:
-                    self.msg.angular.z = -self.speed*2
-            elif seg.word.find("back") > -1:
-                self.msg.linear.x = -self.speed
-                self.msg.angular.z = 0
-            elif seg.word.find("stop") > -1 or seg.word.find("halt") > -1:
-                self.msg = Twist()
+            if seg.word.find("start") > -1:
+                self.pub_.publish('start')
 
-        self.pub_.publish(self.msg)
+            if seg.word.find("halt") > -1:
+                self.pub_.publish('halt')
+
+            if seg.word.find("open") > -1:
+                self.pub_.publish('open')
+
+            if seg.word.find("close") > -1:
+                self.pub_.publish('close')
+
+            if seg.word.find("robot") > -1:
+                self.pub_.publish('robot')
+
 
     def shutdown(self):
         """
         command executed after Ctrl+C is pressed
         """
         rospy.loginfo("Stop ASRControl")
-        self.pub_.publish(Twist())
+        self.pub_.publish(String())
         rospy.sleep(1)
 
 if __name__ == '__main__':    
@@ -130,7 +109,7 @@ if __name__ == '__main__':
         (default: voice_cmd.kwlist)''')
     
     parser.add_argument('--rospub', type=str,
-        default='mobile_base/commands/velocity',
+        default='demo_voice_control/command',
         help='''ROS publisher destination
         (default: mobile_base/commands/velocity)''')
 
